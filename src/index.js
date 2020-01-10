2d710e21a5e650f748ea5a55b5f5c3843ddd3cd6
natassh <natacha.im@gmail.com>
2020-01-10 11:50:36 +0100

    ✨ new organization and cleaning of files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './ui/App/App';
+import * as serviceWorker from './ui/App/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/ui/App/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Title from '../components/Atoms/Title';
+
+import Ribbon from '../../packages/used-stack/Components/Ribbon/Ribbon';
+import Modal from '../../packages/used-stack/Components/Modal/Modal';
+
+import './styles/app.css';
+
+function App() {
+  const [state, setState] = useState({
+    isOpen: false
+  });
+
+  const handleIsOpen = value => {
+    setState({ isOpen: value });
+  };
+
+  const handleCloseModal = () => {
+    setState({ isOpen: false });
+  };
+
+  return (
+    <div className="App">
+      <Title className="Title">El pronóstico del tiempo</Title>
+
+      <Ribbon text="Used stack" onChange={handleIsOpen} />
+      {state.isOpen && <Modal onClose={handleCloseModal} />}
+    </div>
+  );
+}
+
+export default App;
